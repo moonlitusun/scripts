061b3882941e3e65e69f469b45f2406ba70816d6
Roc <moonlitusun@gmail.com>
2023-10-08 09:39:07 +0800

    feat: hugo-generate add -c option

--- hugo/hugo-generate.sh
+++ hugo/hugo-generate.sh
@@ -4,5 +4,12 @@ repo_name="moonlit-private"
 repo_path="${path%%$repo_name*}$repo_name"
 target_path="${path#*$repo_name/}"
 
+if [ "$2" = "-c" ]; then
+  mkdir "./$1"
+  cd "./$1" || exit 1
+  mkdir "./.code"
+  target_path="$target_path/$1"
+fi
+
 cd $repo_path
 hugo new "$target_path/$1.md"
