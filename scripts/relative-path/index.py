fcf0e2e002c8ba2e5d5a1edb298165e36f250c4d
Roc <moonlitusun@gmail.com>
2023-10-26 11:08:21 +0800

    feat:

--- scripts/relative-path/index.py
+++ scripts/relative-path/index.py
@@ -1,1 +1,10 @@
-py = 1
+import os
+import sys
+import pyperclip
+
+current_path = os.path.abspath(sys.argv[0])
+rel_path = os.path.relpath(current_path, sys.argv[1])
+
+path = "{}/{}".format(rel_path, os.path.basename(sys.argv[1]))
+print(path)
+pyperclip.copy(path)
