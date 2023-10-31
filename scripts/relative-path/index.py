4c1a73fc90ffffebe092cb238a094d7216babc77
Roc <moonlitusun@gmail.com>
2023-10-31 15:09:26 +0800

    fix:

--- scripts/relative-path/index.py
+++ scripts/relative-path/index.py
@@ -2,10 +2,8 @@ import os
 import sys
 import pyperclip
 
-# current_path = os.path.abspath(sys.argv[0])
 current_path = os.getcwd()
-rel_path = os.path.relpath(current_path, sys.argv[1])
 
-path = "{}/{}".format(rel_path, os.path.basename(sys.argv[1]))
-print(path)
-pyperclip.copy(path)
+rel_path = os.path.relpath(sys.argv[1], current_path)
+print(rel_path)
+pyperclip.copy(rel_path)
