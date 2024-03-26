701d41b568f4c1eddaf8050fc55285cdda759c47
Roc <moonlitusun@gmail.com>
2024-03-26 14:10:53 +0800

    feat: gps filter upstream remote

--- git/gpa.sh
+++ git/gpa.sh
@@ -2,5 +2,9 @@
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 
 for remote in $(git remote); do
+  if [ "$remote" == "upstream" ]; then
+    continue
+  fi
+
   git push $remote $current_branch
 done
