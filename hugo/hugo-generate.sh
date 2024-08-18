ef739a2a0b067016bc80a67f8cb0aa891f73245a
Roc <moonlitusun@gmail.com>
2024-08-18 15:15:31 +0800

    feat: add change ,hg

--- hugo/hugo-generate.sh
+++ hugo/hugo-generate.sh
@@ -1,21 +1,28 @@
-# cmd _hg
+#!/bin/bash
+
 path="$PWD"
-repo_name="]moonlit-private"
-repo_path="${path%%$repo_name*}$repo_name"
-target_path="${path#*$repo_name/}"
+target_path=${path}
+path_name=$(echo "$1" | sed 's/ /-/g')
+title=$(python3 -c "import sys; print(' '.join(word.capitalize() for word in sys.argv[1].split()))" "$1")
 
 if [ "$2" = "-c" ]; then
-  mkdir "./$1"
-  cd "./$1" || exit 1
+  mkdir "./$path_name"
+  cd "./$path_name" || exit 1
   mkdir "./.code"
-  target_path="$target_path/$1/$1.md"
+  target_path="$target_path/$path_name/$path_name.md"
 else
-  target_path="$target_path/$1.md"
+  target_path="$target_path/$path_name.md"
 fi
 
-echo $target_path
-cd $repo_path
-hugo new "$target_path" -c "./."
+if [ -e "$target_path" ]; then
+  echo "Error: Target path is exist." >&2
+  exit 1
+fi
+
+echo "---
+title: $title
+date: $(date "+%Y-%m-%d %H:%M:%S")
+---" >"$target_path"
 
 sleep 1
 
@@ -25,6 +32,6 @@ sleep 1
 # fi
 
 # use cursor
-if command -v cursor >/dev/null 2>&1; then
-  cursor "$target_path"
-fi
+# if command -v cursor >/dev/null 2>&1; then
+#   cursor "$target_path"
+# fi
